98374bcaefa52307d0e8827c1064970db534b903
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-07-08 14:29:45 -0400

    add humanize to settings

--- fecreader/fecreader/settings.py
+++ fecreader/fecreader/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django.contrib.humanize',
     'formdata',
     'ftpdata',
     'fec_alerts',
