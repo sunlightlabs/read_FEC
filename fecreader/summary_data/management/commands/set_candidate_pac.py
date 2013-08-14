1155687480208f4c2cc70af3672bab8b7c948777
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-08-14 15:56:41 -0400

    id

--- fecreader/summary_data/management/commands/set_candidate_pac.py
+++ fecreader/summary_data/management/commands/set_candidate_pac.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
         summary = SkedE.objects.all().exclude(superceded_by_amendment=True).exclude(candidate_checked__isnull=True).exclude(support_oppose_checked__isnull=True).exclude(expenditure_date_formatted__lt=cycle_start).values('candidate_checked', 'support_oppose_checked','filer_committee_id_number').annotate(total=Sum('expenditure_amount'))
         for summary_line in summary:
             print "Candidate: %s s/o: %s amt: %s committee_id %s" % (summary_line['candidate_checked'], summary_line['support_oppose_checked'], summary_line['total'], summary_line['filer_committee_id_number'])
-            pc, created = Pac_Candidate.objects.get_or_create(candidate__pk=summary_line['candidate_checked'], committee__pk=summary_line['filer_committee_id_number'], support_oppose=summary_line['support_oppose_checked'])
+            pc, created = Pac_Candidate.objects.get_or_create(candidate__pk=summary_line['candidate_checked'], committee__fec_id=summary_line['filer_committee_id_number'], support_oppose=summary_line['support_oppose_checked'])
             pc.total_ind_exp = int(round(summary_line['expenditure_amount']))
             pc.save()
         
