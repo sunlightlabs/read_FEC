f5e1b6482d36b92309ad06fee0be279371ca6a32
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-09-04 16:56:12 -0400

    change queue for dump

--- fecreader/downloads/views.py
+++ fecreader/downloads/views.py
@@ -9,16 +9,14 @@ from django.shortcuts import get_object_or_404, render_to_response
 from reconciliation.utils.json_helpers import render_to_json, render_to_json_via_template
 
 def get_filing(request, filing_number, sked):
-    # should eventually have a home page, or straighten out urls
-    celery_request = dump_filing_sked_celery.apply_async([sked,filing_number], queue='slow',routing_key="slow")    
+    celery_request = dump_filing_sked_celery.apply_async([sked,filing_number], queue='fast',routing_key="fast")    
     task_id = celery_request.id
     return redirect('/download/build_file/%s/' % task_id)
     
     
 
 def get_committee(request, committee_id, sked):
-    # should eventually have a home page, or straighten out urls
-    celery_request = dump_committee_sked_celery.apply_async([sked,committee_id], queue='slow',routing_key="slow")
+    celery_request = dump_committee_sked_celery.apply_async([sked,committee_id], queue='fast',routing_key="fast")
     task_id = celery_request.id
     return redirect('/download/build_file/%s/' % task_id)
     
