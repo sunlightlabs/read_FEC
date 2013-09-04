c52ee5c932f31af436a71776a759ac9c7e20b928
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-09-04 16:01:46 -0400

    and committees

--- fecreader/downloads/urls.py
+++ fecreader/downloads/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.defaults import url, patterns
 
 urlpatterns = patterns('',
         url(r'^filing/(?P<filing_number>[\d]+)/(?P<sked>[ABE])/', 'downloads.views.get_filing'),
+        url(r'^filing/(?P<committee_id>[\d]+)/(?P<sked>[ABE])/', 'downloads.views.get_committee'),
         url(r'^taskstatus/(?P<task_id>[\w\d\-]+)/', 'downloads.views.get_task_status'),
         url(r'^build_file/(?P<task_id>[\w\d\-]+)/', 'downloads.views.build_file'),
 
