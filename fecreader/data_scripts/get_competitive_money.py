e898197d820df618d7b3a3b65b64afc2c13ebf43
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-10-15 14:24:56 -0400

    more details

--- fecreader/data_scripts/get_competitive_money.py
+++ fecreader/data_scripts/get_competitive_money.py
@@ -19,6 +19,6 @@ for office in ['H', 'S']:
     for race in races:
         credible_candidates = Candidate_Overlay.objects.filter(district=race, total_receipts__gte=fundraising_threshold).exclude(not_seeking_reelection=True)
         if len(credible_candidates) > 1:
-            print "\n\nFound multiple candidates in district %s %s %s -- incumbent is %s %s " % (race.state, race.office, race.office_district, race.incumbent_name, race.incumbent_party)
+            print "\n\nFound multiple candidates in district %s %s %s -- Rating is: %s incumbent is %s %s " % (race.state, race.office, race.office_district, race.rothenberg_rating_text, race.incumbent_name, race.incumbent_party)
             for challenger in credible_candidates:
                 print "\tcandidate: |%s| (|%s|) total_receipts|%s| cash:|%s| as of|%s|" % (challenger.name, challenger.party, challenger.total_receipts, challenger.cash_on_hand, challenger.cash_on_hand_date)
