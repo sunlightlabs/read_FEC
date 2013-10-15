a852f31cc8c1e57402e8ca85407bfbab239527fe
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-10-15 14:19:57 -0400

    script to id races

---
+++ fecreader/data_scripts/get_competitive_money.py
@@ -0,0 +1,24 @@
+# How many races have multiple candidates raising big sums of money?
+import sys, os
+
+from django.core.management import setup_environ
+sys.path.append('../fecreader/')
+sys.path.append('../')
+
+import settings
+setup_environ(settings)
+
+from summary_data.models import District, Candidate_Overlay
+
+fundraising_threshold = 100000
+
+for office in ['H', 'S']:
+    # Ignore open seats
+    races = District.objects.filter(office=office, open_seat=False)
+
+    for race in races:
+        credible_candidates = Candidate_Overlay.objects.filter(district=race, total_receipts__gte=fundraising_threshold).exclude(not_seeking_reelection=True)
+        if len(credible_candidates) > 1:
+            print "\n\nFound multiple candidates in district %s %s %s -- incumbent is %s %s " % (race.state, race.office, race.office_district, race.incumbent_name, race.incumbent_party)
+            for challenger in credible_candidates:
+                print "\tcandidate: |%s| (|%s|) total_receipts|%s| cash:|%s| as of|%s|" % (challenger.name, challenger.party, challenger.total_receipts, challenger.cash_on_hand, challenger.cash_on_hand_date)
