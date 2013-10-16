d291bbd28205478739376b9f253078d2cbfac437
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-10-16 12:57:05 -0400

    sticky urls for house pages

--- fecreader/public_views/datapages/views.py
+++ fecreader/public_views/datapages/views.py
@@ -78,7 +78,7 @@ def senate(request):
     # Give up on ORM for data; we're not willing to enforce all the relationships required for them
     districts = District.objects.filter(office='S')
 
-    legislators = Candidate_Overlay.objects.filter(office='S').filter(Q(cash_on_hand__gte=1000)|Q(is_incumbent=True)).select_related('district')
+    legislators = Candidate_Overlay.objects.filter(office='S').filter(Q(cash_on_hand__gte=1000)|Q(is_incumbent=True)).select_related('district').order_by('-cash_on_hand')
 
     return render_to_response('datapages/senate_legislator_list.html',
         {
@@ -98,7 +98,7 @@ def house(request):
     explanatory_text="Fundraising totals are for the entire election cycle for current U.S. House members and House candidates who reported having $1,000 or more. Only candidates actually running in the current cycle who filed a statement of candidacy are included. If we included anyone who isn't running--or missed anyone who is, please <a href='mailto:realtimefec@sunlightfoundation.com'>let us know</a>. Please note these totals reflect current FEC filings and may not match the summarized data available elsewhere on Influence Explorer."
     # Give up on ORM for data; we're not willing to enforce all the relationships required for them
 
-    legislators = Candidate_Overlay.objects.filter(office='H').filter(Q(cash_on_hand__gte=1000)|Q(is_incumbent=True)).select_related('district')
+    legislators = Candidate_Overlay.objects.filter(office='H').filter(Q(cash_on_hand__gte=1000)|Q(is_incumbent=True)).select_related('district').order_by('-cash_on_hand')
     
     districts = District.objects.filter(office='H')
 
