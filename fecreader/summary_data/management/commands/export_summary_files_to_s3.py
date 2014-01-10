5e7f5fe0fa9d9d0b42faa3ece7d115a57b793c3e
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-01-10 10:12:44 -0500

    add non-individual donations to summary file dump tasks

--- fecreader/summary_data/management/commands/export_summary_files_to_s3.py
+++ fecreader/summary_data/management/commands/export_summary_files_to_s3.py
@@ -9,7 +9,7 @@ from django.conf import settings
 from summary_data.utils.update_utils import set_update
 from summary_data.utils.summary_utils import write_all_candidates, write_all_webks
 
-from formdata.utils.dump_utils import dump_big_contribs
+from formdata.utils.dump_utils import dump_big_contribs, dump_big_non_indiv_contribs
 
 AWS_ACCESS_KEY_ID = getattr(settings, 'AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY =  getattr(settings, 'AWS_SECRET_ACCESS_KEY')
@@ -42,35 +42,45 @@ class Command(BaseCommand):
         filename = "candidates.csv" 
         webk_filename = "all_webk.csv"
         contrib_filename = 'superpac_contribs.csv'
+        nonindiv_contrib_filename = 'nonindiv_nonpac_superpac_contribs.csv'
         
         local_file = "%s/%s" % (CSV_EXPORT_DIR, filename)
         local_webk_file = "%s/%s" % (CSV_EXPORT_DIR, webk_filename)
         local_contrib_file = "%s/%s" % (CSV_EXPORT_DIR, contrib_filename)
-
+        local_nonindiv_contrib_file = "%s/%s" % (CSV_EXPORT_DIR, nonindiv_contrib_filename)
+        
+        
+        dump_big_non_indiv_contribs(local_nonindiv_contrib_file)
         write_all_candidates(local_file)
         write_all_webks(local_webk_file)
         dump_big_contribs(local_contrib_file)
         
         
         # need to gzip these
-        gzip_cmd = "gzip -f %s %s %s" % (local_file, local_webk_file, local_contrib_file)
+        gzip_cmd = "gzip -f %s %s %s %s" % (local_file, local_webk_file, local_contrib_file, local_nonindiv_contrib_file)
         filename_zipped = filename + ".gz"
         filename_webk_zipped = webk_filename + ".gz"
         filename_contrib_zipped = contrib_filename + ".gz"
+        filename_nonindiv_contrib_zipped = nonindiv_contrib_filename + ".gz"
         
         local_file_zipped = local_file + ".gz"
         local_webk_file_zipped = local_webk_file + ".gz"
         local_contrib_file_zipped = local_contrib_file + ".gz"
+        local_nonindiv_contrib_file_zipped = local_nonindiv_contrib_file + ".gz"
+        
         
         # old style os.system just works - subprocess sucks. 
         proc = os.system(gzip_cmd)
         s3_path = "%s/%s" % (AWS_BULK_EXPORT_PATH,filename_zipped)
         webk_s3_path = "%s/%s" % (AWS_BULK_EXPORT_PATH,filename_webk_zipped)
         contrib_s3_path = "%s/%s" % (AWS_BULK_EXPORT_PATH,filename_contrib_zipped)
+        nonindiv_s3_path = "%s/%s" % (AWS_BULK_EXPORT_PATH,filename_nonindiv_contrib_zipped)
         
         push_to_s3(local_file_zipped, AWS_STORAGE_BUCKET_NAME, s3_path)
         push_to_s3(local_webk_file_zipped, AWS_STORAGE_BUCKET_NAME, webk_s3_path)
         push_to_s3(local_contrib_file_zipped, AWS_STORAGE_BUCKET_NAME, contrib_s3_path)
+        push_to_s3(local_nonindiv_contrib_file_zipped, AWS_STORAGE_BUCKET_NAME, nonindiv_s3_path)
+
         
         # if we didn't die, set the update time
         set_update(SUMMARY_EXPORT_KEY)
