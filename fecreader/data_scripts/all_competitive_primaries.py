3527af379c389b7644f7952d89c7c5649305512e
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-02-10 12:10:39 -0500

    start of script to look at all competitive primaries

---
+++ fecreader/data_scripts/all_competitive_primaries.py
@@ -0,0 +1,51 @@
+# How many primaries will be hard-fought?
+
+import sys, os
+
+from django.core.management import setup_environ
+sys.path.append('../fecreader/')
+sys.path.append('../')
+
+import settings
+setup_environ(settings)
+
+from summary_data.models import District, Candidate_Overlay
+
+house_fundraising_threshold = 100000
+senate_fundraising_threshold = 200000
+
+def comment_print(message):
+    print "###  " + message + "\n"
+########
+# Deal with jungle primary states first: CA, WA, LA
+########
+
+comment_print("Dealing with jungle primary states")
+
+for state in ['CA', 'LA', 'WA']:
+    comment_print("Dealing with state %s" % (state))
+    
+    races = District.objects.filter(state=state, election_year=2014)
+    
+    for race in races:
+        
+        candidates = Candidate_Overlay.objects.filter(district=race).exclude(not_seeking_reelection=True)
+        if race.office == 'H':
+            candidates = candidates.filter(total_receipts__gte=house_fundraising_threshold)
+        else:
+            candidates = candidates.filter(total_receipts__gte=senate_fundraising_threshold)
+            
+
+        
+        if len(candidates) > 1:
+            comment_print("Multiple candidates found !")
+
+            print "Office =%s District =%s incumbent=%s incumbent party = %s rating: %s (%s)" % (race.office, race.office_district, race.incumbent_name, race.incumbent_party, race.rothenberg_rating_text, race.rothenberg_rating_id)
+            
+            for candidate in candidates:
+                print "\tcandidate: %s party: %s incumbent: %s (%s) total raised: %s cash on hand %s (as of %s)" % (candidate.name, candidate.party, candidate.is_incumbent, candidate.total_receipts, candidate.cash_on_hand, candidate.cash_on_hand_date )
+            print "\n\n"
+            
+    
+    
+    
