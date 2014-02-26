ff9ffa9e6d0214eb4e6af1603472a7d784e46822
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-02-26 11:10:39 -0500

    primary script tweaks

--- fecreader/data_scripts/all_competitive_primaries.py
+++ fecreader/data_scripts/all_competitive_primaries.py
@@ -14,8 +14,12 @@ from summary_data.models import District, Candidate_Overlay
 house_fundraising_threshold = 100000
 senate_fundraising_threshold = 200000
 
+house_cash_on_hand_threshold = 20000
+senate_cash_on_hand_threshold = 40000
+
 def comment_print(message):
     print "###  " + message + "\n"
+    
 ########
 # Deal with jungle primary states first: CA, WA, LA
 ########
@@ -31,9 +35,9 @@ for state in ['CA', 'LA', 'WA']:
         
         candidates = Candidate_Overlay.objects.filter(district=race).exclude(not_seeking_reelection=True)
         if race.office == 'H':
-            candidates = candidates.filter(total_receipts__gte=house_fundraising_threshold)
+            candidates = candidates.filter(total_receipts__gte=house_fundraising_threshold,cash_on_hand__gte=house_cash_on_hand_threshold)
         else:
-            candidates = candidates.filter(total_receipts__gte=senate_fundraising_threshold)
+            candidates = candidates.filter(total_receipts__gte=senate_fundraising_threshold,cash_on_hand__gte=senate_cash_on_hand_threshold)
             
 
         # There are two berths, so its competitive only if there are three spots
@@ -51,5 +55,22 @@ for state in ['CA', 'LA', 'WA']:
 
 races = District.objects.filter(election_year=2014).exclude(state__in=['CA', 'WA', 'LA'])
 
-
+for race in races:
+    
+    candidates = Candidate_Overlay.objects.filter(district=race).exclude(not_seeking_reelection=True)
+    if race.office == 'H':
+        candidates = candidates.filter(total_receipts__gte=house_fundraising_threshold,cash_on_hand__gte=house_cash_on_hand_threshold)
+    else:
+        candidates = candidates.filter(total_receipts__gte=senate_fundraising_threshold,cash_on_hand__gte=senate_cash_on_hand_threshold)
+        
+    for party in ['D', 'R']:
+        party_candidates = candidates.filter(party=party)
+        if len(party_candidates) > 2:
+            print "State=%s Office =%s District =%s incumbent=%s incumbent party = %s is open %s rating: %s (%s)" % (race.state, race.office, race.office_district, race.incumbent_name, race.incumbent_party, race.open_seat,  race.rothenberg_rating_text, race.rothenberg_rating_id)
+            
+            for candidate in party_candidates:
+                print "\tcandidate: %s party: %s incumbent: %s total raised: %s cash on hand %s (as of %s)" % (candidate.name, candidate.party, candidate.is_incumbent, candidate.total_receipts, candidate.cash_on_hand, candidate.cash_on_hand_date )
+            print "\n\n"
+            
+            
     
