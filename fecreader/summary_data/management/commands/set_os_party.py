f093999db651309acf8feb07e69ba1c78b89b2c1
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-03-24 16:22:08 -0400

    test script to set party alignment

---
+++ fecreader/summary_data/management/commands/set_os_party.py
@@ -0,0 +1,38 @@
+""" Set party leaning for outside spending groups. """
+
+from datetime import date
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Sum
+from formdata.models import SkedE
+from summary_data.models import Candidate_Overlay, Committee_Overlay
+
+
+
+class Command(BaseCommand):
+    help = "Create race aggregates"
+    requires_model_validation = False
+    
+    def handle(self, *args, **options):
+        # Leave out committees we've verified by hand
+        committee_list = Committee_Overlay.objects.filter(total_indy_expenditures__gte=1000).exclude(political_orientation_verified=True)
+        for committee in committee_list:
+            if committee.ie_support_dems > 0 and not committee.ie_support_reps > 0:
+                committee.political_orientation = 'D'
+                print "Supports only Ds %s" % (committee.name)
+                committee.save()                
+            elif committee.ie_support_reps > 0 and not committee.ie_support_dems > 0:
+                committee.political_orientation = 'R'
+                print "Supports only Rs %s" % (committee.name)
+                committee.save()
+            
+            # if they've only spend negatively, they might just be playing in the primary. 
+            elif committee.ie_oppose_reps > 0 and not committee.ie_oppose_dems > 0 and not committee.ie_support_reps > 0 and not committee.ie_support_dems > 0:
+                print "Purely negative againsts Rs -- not setting. %s" % (committee.name)
+            
+            # if they've only spend negatively, they might just be playing in the primary. 
+            elif not committee.ie_oppose_reps > 0 and committee.ie_oppose_dems > 0 and not committee.ie_support_reps > 0 and not  committee.ie_support_dems > 0:
+                print "Purely negative againsts Ds -- not setting. %s" % (committee.name)
+            else:
+                print "***** Other. %s" % (committee.name)
+        
+        
