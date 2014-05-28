f3a098e03697430baa484d11703013c62782969c
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-05-28 15:13:08 -0400

    tweak

--- fecreader/summary_data/management/commands/get_state_candidate_status.py
+++ fecreader/summary_data/management/commands/get_state_candidate_status.py
@@ -28,12 +28,12 @@ class Command(BaseCommand):
         #print "Checking candidate status='%s'" % (state)
         candidate_list = []
 
-        races = District.objects.filter(state=state, election_year=2014)
+        races = District.objects.filter(state=state, election_year=2014).order_by('office', 'office_district')
     
         for race in races:
                 #print "Handling race %s" % (race)
-        
-                candidates = Candidate_Overlay.objects.filter(district=race).exclude(not_seeking_reelection=True).order_by('office', 'office_district', 'party', 'name')
+                  
+                candidates = Candidate_Overlay.objects.filter(district=race).exclude(not_seeking_reelection=True).order_by('party', 'name')
                 
                 for candidate in candidates:
                     candidate_list.append({"id":candidate.pk, "fec_id":candidate.fec_id, "is_incumbent":candidate.is_incumbent, "name":candidate.name, "party":candidate.party, "office":candidate.office, "district":candidate.office_district, "candidate_status":candidate.candidate_status})
