577cb3e4b41fd71485b5cd71b2006abcf316b68a
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-05-28 13:43:30 -0400

    another data entry management script

---
+++ fecreader/summary_data/management/commands/get_state_candidate_status.py
@@ -0,0 +1,58 @@
+# show candidate status as json object suitable for human editing. 
+
+from optparse import make_option
+
+from datetime import datetime, date
+
+from django.template import Template
+from django.template.loader import get_template
+from django.template import Context
+from django.conf import settings
+from django.db.models import Q
+
+from summary_data.models import District, Candidate_Overlay, Election
+
+from django.core.management.base import BaseCommand, CommandError
+
+today = date.today()
+
+
+def comment_print(message):
+    print "###  " + message + "\n"
+    
+########
+# Deal with jungle primary states first: CA, WA, LA
+########
+
+
+class Command(BaseCommand):
+    help = "Regenerate the static competitive primary page."
+    requires_model_validation = False
+    
+    
+    option_list = BaseCommand.option_list + (
+            make_option('--state',
+                        action='store',
+                        dest='state',
+                        help="State to process"),
+            )
+    
+    def handle(self, *args, **options):
+
+        state = options['state']
+        assert state, "No state given"
+        print "Checking candidate status='%s'" % (state)
+        candidate_list = []
+
+        races = District.objects.filter(state=state, election_year=2014)
+    
+        for race in races:
+                #print "Handling race %s" % (race)
+        
+                candidates = Candidate_Overlay.objects.filter(district=race).exclude(not_seeking_reelection=True).order_by('-cash_on_hand')
+                
+                for candidate in candidates:
+                    candidate_list.append({"id":candidate.pk, "fec_id":candidate.fec_id, "is_incumbent":candidate.is_incumbent, "name":candidate.name, "party":candidate.party, "office":candidate.office, "district":candidate.office_district, "status":candidate.candidate_status})
+
+
+        print candidate_list
