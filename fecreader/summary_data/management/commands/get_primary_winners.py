670b2ca282704d75b48c52263dcec9e47463bee0
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-09-08 16:29:35 -0400

    add convention as a way to lose

--- fecreader/summary_data/management/commands/get_primary_winners.py
+++ fecreader/summary_data/management/commands/get_primary_winners.py
@@ -27,13 +27,14 @@ def process_state(state):
         for race in races:
             #print "Handling race %s" % (race)
     
-            candidates = Candidate_Overlay.objects.filter(district=race).exclude(not_seeking_reelection=True).exclude(candidate_status__in=['W', 'LP', 'SP', 'SR', 'SG', 'SX', 'PR']).order_by('-cash_on_hand')
+            candidates = Candidate_Overlay.objects.filter(district=race).exclude(not_seeking_reelection=True).exclude(candidate_status__in=['W', 'LP', 'SP', 'SR', 'SG', 'SX', 'PR', 'LC']).order_by('-cash_on_hand')
             
 
             # There are two berths, so its competitive only if there are three spots
             if len(candidates) > 2:
                 comment_print("Too many candidates remaining in jungle primary: %s %s %s" % (state, race.office, race.office_district))
-                
+                    for candidate in candidates:
+                        print "\t%s %s %s %s %s %s" % (candidate.name, candidate.fec_id, candidate.office, candidate.office_district, candidate.party, candidate.candidate_status)
 
 
     else:
@@ -45,7 +46,7 @@ def process_state(state):
             #print "Handling race %s" % (race)
             
             for party in ['D', 'R']:
-                candidates = Candidate_Overlay.objects.filter(district=race, party=party).exclude(not_seeking_reelection=True).exclude(candidate_status__in=['W', 'LP', 'SP', 'SR', 'SG', 'SX', 'PR']).order_by('-cash_on_hand')
+                candidates = Candidate_Overlay.objects.filter(district=race, party=party).exclude(not_seeking_reelection=True).exclude(candidate_status__in=['W', 'LP', 'SP', 'SR', 'SG', 'SX', 'PR', 'LC']).order_by('-cash_on_hand')
                 if len(candidates) > 1:
                     print "Too many candidates still in race after %s primary: %s %s %s" % (party, state, race.office, race.office_district)    
                     for candidate in candidates:
