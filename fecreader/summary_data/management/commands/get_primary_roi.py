0eb2cf6456cf778d6e2ee1cc05880b7cd5216692
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-09-10 11:37:46 -0400

    add starter

---
+++ fecreader/summary_data/management/commands/get_primary_roi.py
@@ -0,0 +1,35 @@
+from datetime import date
+from django.core.management.base import BaseCommand, CommandError 
+from django.db.models import Sum
+
+
+election_day_2014 = date(2014,11,4)
+cycle_start = date(2013,1,1)
+
+from summary_data.models import *
+from formdata.models import SkedE
+
+class Command(BaseCommand):
+ 
+    def handle(self, *args, **options): 
+        outside_spenders = Committee_Overlay.objects.filter(total_indy_expenditures__gte=50000)
+        primary_ies = SkedE.objects.filter(superceded_by_amendment=False, expenditure_date__gte=cycle_start, election_code__in=['P2014', 'R2014'])
+        spender_list = []
+        for outside_spender in outside_spenders:
+            print "processing %s" % (outside_spender)
+            
+            their_ies = primary_ies.filter(filer_committee_id_number=outside_spender.fec_id)
+            
+            total = their_ies.aggregate(tot_ies=Sum('expenditure_amount'))['tot_ies']
+            oppose_dems = their_ies.filter(candidate_party_checked='D', support_oppose_checked='O').aggregate(tot_ies=Sum('expenditure_amount'))['tot_ies']
+            oppose_reps = their_ies.filter(candidate_party_checked='R', support_oppose_checked='O').aggregate(tot_ies=Sum('expenditure_amount'))['tot_ies']
+            support_dems = their_ies.filter(candidate_party_checked='D', support_oppose_checked='S').aggregate(tot_ies=Sum('expenditure_amount'))['tot_ies']
+            support_reps = their_ies.filter(candidate_party_checked='R', support_oppose_checked='S').aggregate(tot_ies=Sum('expenditure_amount'))['tot_ies']
+            
+            this_spender_data = {'name':outside_spender.name, 'url':outside_spender.get_absolute_url(), 'total_ies':total, 'oppose_dems':oppose_dems, 'oppose_reps':oppose_reps, 'support_dems':support_dems, 'support_reps':support_reps, 'type':outside_spender.display_type(), 'orientation':outside_spender.display_political_orientation(), 'ie_url':outside_spender.get_filtered_ie_url()}
+            print this_spender_data
+            spender_list.append(this_spender_data)
+            
+        
+        
+        
