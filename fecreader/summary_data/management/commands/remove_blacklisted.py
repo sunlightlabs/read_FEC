5c3b5a06b4e1b2991d5de6f3b70352dbb413e240
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-10-03 11:06:20 -0400

    more complete removal

---
+++ fecreader/summary_data/management/commands/remove_blacklisted.py
@@ -0,0 +1,26 @@
+# Remove problematic entities.
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+from summary_data.models import Committee_Overlay, Candidate_Overlay, Committee_Time_Summary, Authorized_Candidate_Committees
+from fec_alerts.models import WebK
+
+blacklisted_committees = ['C00507947']
+blacklisted_candidates = ['P20003851']
+
+class Command(BaseCommand):
+    help = "Set "
+    requires_model_validation = False
+    
+    def handle(self, *args, **options):
+        for fecid in blacklisted_committees:
+            Committee_Overlay.objects.filter(fec_id=fecid).delete()
+            Committee_Time_Summary.objects.filter(com_id=fecid).delete()
+            Authorized_Candidate_Committees.objects.filter(committee_id=fecid).delete()
+            WebK.objects.filter(com_id=fecid).delete()
+        
+        for fecid in blacklisted_candidates:
+            Candidate_Overlay.objects.filter(fec_id=fecid).delete()
+            Authorized_Candidate_Committees.objects.filter(candidate_id=fecid).delete()
+            
