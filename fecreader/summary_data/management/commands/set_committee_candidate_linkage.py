f84a5f62aee10889272c00fe586e3806a69a67c1
Jacob Fenton <jfenton@sunlightfoundation.com>
2015-05-01 13:27:51 -0400

    cycle for sccl

--- fecreader/summary_data/management/commands/set_committee_candidate_linkage.py
+++ fecreader/summary_data/management/commands/set_committee_candidate_linkage.py
@@ -3,30 +3,37 @@
 from django.core.management.base import BaseCommand, CommandError
 
 from summary_data.models import Authorized_Candidate_Committees, Candidate_Overlay, Committee_Overlay
+from django.conf import settings
 
+try:
+    CURRENT_CYCLE = settings.CURRENT_CYCLE
+except:
+    print "Missing current cycle list. Defaulting to 2016. "
+    CURRENT_CYCLE = ['2016']
+    
 
 class Command(BaseCommand):
     help = "Sets the curated_candidate_overlay field in committee_overlay from the Authorized_Candidate_Committees model"
     requires_model_validation = False
     
     def handle(self, *args, **options):
-        accs = Authorized_Candidate_Committees.objects.all()
+        accs = Authorized_Candidate_Committees.objects.filter(cycle=CURRENT_CYCLE)
         for acc in accs:
             como = cando = None
             
             try:
-                como = Committee_Overlay.objects.get(fec_id=acc.committee_id)
+                como = Committee_Overlay.objects.get(fec_id=acc.committee_id, cycle=CURRENT_CYCLE)
             except Committee_Overlay.DoesNotExist:
-                print "Missing committee overlay for %s %s" % (acc.committee_name, acc.committee_id)
+                print "Missing committee overlay for %s %s and cycle %s" % (acc.committee_name, acc.committee_id, CURRENT_CYCLE)
                 continue
                 
             try:
-                cando = Candidate_Overlay.objects.get(fec_id=acc.candidate_id)
+                cando = Candidate_Overlay.objects.get(fec_id=acc.candidate_id, cycle=CURRENT_CYCLE)
             except Candidate_Overlay.DoesNotExist:
                 #print "Missing candidate overlay for candidate--committee name is:%s committee_id is: %s" % (acc.committee_name, acc.candidate_id)
                 continue
             except Candidate_Overlay.MultipleObjectsReturned:
-                print "Multiple candidate overlays returned for candidate--committee name is:%s committee_id is: %s" % (acc.committee_name, acc.candidate_id)
+                print "Multiple candidate overlays returned for candidate--committee name is:%s committee_id is: %s during cycle %s" % (acc.committee_name, acc.candidate_id, CURRENT_CYCLE)
                 
             como.curated_candidate = cando
             como.save()
