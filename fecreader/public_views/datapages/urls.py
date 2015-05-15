15d5ffa9fd09171f1d96b37ea70dbfd5f9161a76
Jacob Fenton <jfenton@sunlightfoundation.com>
2015-05-15 10:39:24 -0400

    wip

--- fecreader/public_views/datapages/urls.py
+++ fecreader/public_views/datapages/urls.py
@@ -30,7 +30,7 @@ urlpatterns = patterns('',
         
         
         url(r'^candidate/(?P<cycle>20\d\d)\/[\w-]+\/(?P<candidate_id>[\w\d]+)\/?$', 'public_views.datapages.views.candidate_cycle'),
-        url(r'^candidate/[\w-]+\/(?P<candidate_id>[\w\d]+)\/?$', 'public_views.datapages.views.candidate'),                
+        url(r'^candidate/(?P<slug>[\w-]+)\/(?P<candidate_id>[\w\d]+)\/?$', 'public_views.datapages.views.candidate'),                
         url(r'^race/(?P<cycle>\d\d\d\d)\/H\/(?P<state>\w\w)\/(?P<district>\d+)\/', 'public_views.datapages.views.house_race'),
         url(r'^race/(?P<cycle>\d\d\d\d)\/S\/(?P<state>\w\w)\/(?P<term_class>\d+)\/', 'public_views.datapages.views.senate_race'),
         url(r'^race_id/(?P<race_id>\d+)\/$', 'public_views.datapages.views.race_id_redirect'),
--- fecreader/public_views/datapages/views.py
+++ fecreader/public_views/datapages/views.py
@@ -511,53 +511,10 @@ def committee_cycle(request, cycle, committee_id):
 """ to be replaced with candidate_cycle """
 
 @cache_page(LONG_CACHE_TIME)
-def candidate(request, candidate_id):
-    candidate_overlay = get_object_or_404(Candidate_Overlay, fec_id=candidate_id)
-    title = "%s (%s) " % (candidate_overlay.name, candidate_overlay.party)
+def candidate(request, slug, candidate_id):
+    # this form of url was used for 2014, so redirect there.
+    return redirect("/candidate/2014/%s/%s/" % (slug, candidate_id))
     
-    authorized_committee_list = Authorized_Candidate_Committees.objects.filter(candidate_id=candidate_id)
-    committee_list = [x.get('committee_id') for x in authorized_committee_list.values('committee_id')]
-    
-    report_list = Committee_Time_Summary.objects.filter(com_id__in=committee_list, coverage_from_date__gte=this_cycle_start).order_by('-coverage_through_date')
-    
-    
-    end_of_coverage_date = None
-    recent_report_list = None
-    if report_list:
-        end_of_coverage_date = report_list[0].coverage_through_date
-        
-    
-    recent_report_total = 0
-    if end_of_coverage_date:
-        recent_report_list = new_filing.objects.filter(fec_id__in=committee_list, coverage_from_date__gte=end_of_coverage_date, form_type__in=['F6', 'F6A', 'F6N']).exclude(is_superceded=True)
-        if recent_report_list:
-            recent_report_total = recent_report_list.aggregate(spending_total=Sum('tot_raised'))['spending_total']
-        
-    else:
-        recent_report_list = new_filing.objects.filter(fec_id__in=committee_list, coverage_from_date__gte=this_cycle_start, form_type__in=['F6', 'F6A', 'F6N']).exclude(is_superceded=True)
-    
-    
-    # are their outside groups who've spent for/against this candidate? 
-    outside_spenders = Pac_Candidate.objects.filter(candidate=candidate_overlay, total_ind_exp__gte=1000).select_related('committee')
-    
-    recent_ies = None
-    if outside_spenders:
-        recent_ies = SkedE.objects.filter(candidate_checked=candidate_overlay, expenditure_amount__gte=5000, superceded_by_amendment=False, expenditure_date_formatted__gte=this_cycle_start).select_related('candidate_checked').order_by('-expenditure_date_formatted')[:10]
-    
-    
-    return render_to_response('datapages/candidate.html',
-        {
-        'title':title,
-        'report_list':report_list,
-        'candidate':candidate_overlay,
-        'authorized_committee_list':authorized_committee_list,
-        'outside_spenders':outside_spenders,
-        'recent_report_list':recent_report_list,
-        'recent_ies':recent_ies,
-        'recent_report_total':recent_report_total,
-        }, 
-        context_instance=RequestContext(request)
-    )
 
 
 
@@ -602,6 +559,10 @@ def candidate_cycle(request, candidate_id, cycle):
         recent_ies = SkedE.objects.filter(candidate_checked=candidate_overlay, expenditure_amount__gte=5000, superceded_by_amendment=False, expenditure_date_formatted__gte=cycle_endpoints['start'], expenditure_date_formatted__lte=cycle_endpoints['end'] ).select_related('candidate_checked').order_by('-expenditure_date_formatted')[:10]
 
 
+    # figure out which cycles are available. The current one goes first, because it is being displayed.     
+    previous_cycles = Candidate_Overlay.objects.filter(fec_id=candidate_id).exclude(cycle=cycle)
+    cycle_list = [candidate_overlay] + list(previous_cycles)
+
     return render_to_response('datapages/candidate.html',
         {
         'title':title,
@@ -612,6 +573,7 @@ def candidate_cycle(request, candidate_id, cycle):
         'recent_report_list':recent_report_list,
         'recent_ies':recent_ies,
         'recent_report_total':recent_report_total,
+        'cycle_list':cycle_list,
         }, 
         context_instance=RequestContext(request)
     )
