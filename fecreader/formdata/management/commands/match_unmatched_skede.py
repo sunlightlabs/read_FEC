0bd939fa122ff5c405c8b7b4798a27a13cfca1ad
Jacob Fenton <jfenton@sunlightfoundation.com>
2015-06-11 10:47:36 -0400

    epoch start for skede matching.

--- fecreader/formdata/management/commands/match_unmatched_skede.py
+++ fecreader/formdata/management/commands/match_unmatched_skede.py
@@ -6,7 +6,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from process_skede_lines import attach_ie_target
 from formdata.models import SkedE
-
+from datetime import date
 
 
 class Command(BaseCommand):
@@ -14,6 +14,6 @@ class Command(BaseCommand):
     requires_model_validation = False
     
     def handle(self, *args, **options):
-        skedes_to_process = SkedE.objects.filter(candidate_checked__isnull=True)
+        skedes_to_process = SkedE.objects.filter(candidate_checked__isnull=True, effective_date__gte=date(2013,1,1))
         for skede_to_process in skedes_to_process:
             attach_ie_target(skede_to_process)
