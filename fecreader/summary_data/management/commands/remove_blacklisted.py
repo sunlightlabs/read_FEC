4719eeef6254e2137c67920184207b20e28922a6
Jacob Fenton <jfenton@sunlightfoundation.com>
2015-06-18 22:03:21 -0400

    blacklisted newfilings

--- fecreader/summary_data/management/commands/remove_blacklisted.py
+++ fecreader/summary_data/management/commands/remove_blacklisted.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 
 from summary_data.models import Committee_Overlay, Candidate_Overlay, Committee_Time_Summary, Authorized_Candidate_Committees
-from fec_alerts.models import WebK
+from fec_alerts.models import WebK, new_filing
 
 blacklisted_committees = ['C00507947', 'C00428599']
 blacklisted_candidates = ['P20003851', 'P80003205']
@@ -19,6 +19,7 @@ class Command(BaseCommand):
             Committee_Time_Summary.objects.filter(com_id=fecid).delete()
             Authorized_Candidate_Committees.objects.filter(committee_id=fecid).delete()
             WebK.objects.filter(com_id=fecid).delete()
+            new_filing.objects.filter(fec_id=fecid).delete()
         
         for fecid in blacklisted_candidates:
             Candidate_Overlay.objects.filter(fec_id=fecid).delete()
