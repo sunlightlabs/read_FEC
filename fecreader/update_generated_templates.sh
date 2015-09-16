576b4ef523d215badb170fd701cc3191e7b3cba6
Bob Lannon <blannon@gmail.com>
2015-09-16 12:48:27 -0400

    generalizing

--- fecreader/update_generated_templates.sh
+++ fecreader/update_generated_templates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-MY_VENV=/Users/nikole/.virtualenvs/rtfc
+MY_VENV=$HOME/.virtualenvs/rtfc
 MY_PYTHON=$MY_VENV/bin/python
-PROJ_ROOT='/Users/nikole/Documents/git_projects/read_FEC/fecreader'
+PROJ_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 $MY_PYTHON $PROJ_ROOT/manage.py regen_results
 $MY_PYTHON $PROJ_ROOT/manage.py regen_roi
